349be82e1727176433e8a3cfe31d13226e8677be
Alumno06 <123@123.com>
2018-12-06 21:31:24 -0500

    asdasda

--- ProyWebAplicacionesEmpresariales/src/java/Controlador/ClientesController.java
+++ ProyWebAplicacionesEmpresariales/src/java/Controlador/ClientesController.java
@@ -25,13 +25,14 @@ public class ClientesController extends BaseController {
     @Override
     protected void postEditar(HttpServletRequest request) {
         super.postEditar(request); //To change body of generated methods, choose Tools | Templates.
-        //id_cliente=Integer.parseInt(request.getParameter("txtid_cliente"));
+        Integer id_cliente =Integer.parseInt(request.getParameter("id_cliente"));
         String razSoc = request.getParameter("txtRazSocail");
         String doc = request.getParameter("txtTipoDoc");
         String numDoc = request.getParameter("txtNumDoc");
         String email = request.getParameter("txtMail");
         //c.setId_cliene(id_cliente);
         Cliente c=new Cliente();
+        c.setId_cliene(id_cliente);
         c.setRazon_social(razSoc);
         c.setTip_doc(doc);
         c.setNum_doc(numDoc);
@@ -59,8 +60,7 @@ public class ClientesController extends BaseController {
     protected void getEditar(HttpServletRequest request) {
         super.getEditar(request); //To change body of generated methods, choose Tools | Templates.
         request.setAttribute("idcli", request.getParameter("id_cliente"));
-        
-        Cliente cliente = dao.list(Integer.parseInt(request.getParameter("id_cliente")));
+        request.getSession().setAttribute("id_cliente", request.getParameter("id_cliente"));
     }
 
 }
